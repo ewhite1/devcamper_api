9a492db60461b36be63c383b028469b368253340
Edward White <edwardwhite1@outlook.com>
2020-02-10 19:03:31 -0600

    added mongoDB, mongoose and unhandled promise rejections to project

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-// middleware files
+const connectDB = require('./config/db')
+// const logger = require('./middleware/logger')
 
-const logger = require('./middleware/logger')
-//route files
-const bootcamps = require('./routes/bootcamps')
 //load env vars
 dotenv.config({path: './config/config.env'})
+
+// connection to Database
+connectDB()
+//route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -21,4 +24,11 @@ app.use('/api/v1/bootcamps/', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  //close server and exit process
+  server.close(() => process.exit(1))
+})
